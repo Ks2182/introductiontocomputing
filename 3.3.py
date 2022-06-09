24e34bccbaa9edbc481600d676e3e6ddb686d056
Ks2182 <103925458+Ks2182@users.noreply.github.com>
2022-06-09 23:13:45 +0530

    Update 3.3.py

--- 3.3.py
+++ 3.3.py
@@ -1,25 +1,26 @@
+#a
 import math
 print("(Qa)")
 a=(3+4)*5
 print("(3+4)*5=",a)
-
+#b
 print("(Qb)")
 n=int(input("Enter value of n:"))
 b=n*(n-1)/2
 print("n*(n-1)/2=",b)
-
+#c
 print("(Qc)")
 r=int(input("Enter value of r:"))
 c=4*(math.pi)*r**2
 print("Value:",c)
-
+#d
 print("(Qd)")
 r=int(input("Enter value of r:"))
 a=int(input("Enter value of a:"))
 b=int(input("Enter value of b:"))
 d=math.sqrt(r*(math.cos(a))**2+r*(math.sin(b))**2)
 print("Value:",d)
-
+#e
 print("(Qe)")
 y1=int(input("Enter value of y1:",))
 y2=int(input("Enter value of y2:",))
