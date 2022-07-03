7eba23bb86c17981d7d817716afec41511bab000
Ks2182 <103925458+Ks2182@users.noreply.github.com>
2022-07-03 23:26:01 +0530

    Add files via upload

---
+++ run program 6.6.py
@@ -0,0 +1,13 @@
+Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: /Users/rajeevkumarsao/Documents/assignment 6/program 6.6.py =====
+
+Student ID: 21102020
+Student Name: $ Kavya Sao
+
+Student ID: 21102020
+Student Name: $ Kavya Sao
+
+Student Name: $ Kavya Sao
+Student Class: $ 1st Year
